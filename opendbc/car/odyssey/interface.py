defdb583587929483dc74d6408652b2499715030
Quantizr (Jimmy) <9859727+Quantizr@users.noreply.github.com>
2025-07-08 10:35:49 -0700

    closeer to old tuning

--- opendbc/car/odyssey/interface.py
+++ opendbc/car/odyssey/interface.py
@@ -37,11 +37,11 @@ class CarInterface(CarInterfaceBase):
       xs = x - d
       return sig(a * xs) * b + c * xs + e
 
-    sigmoidSharpness = 4.5
-    sigmoidTorqueGain = 1.0
-    latAccelFactor = 0.14
-    horizontalOffset = -0.20
-    verticalOffset = -0.16
+    sigmoidSharpness = 5.0
+    sigmoidTorqueGain = 0.95
+    latAccelFactor = 0.16
+    horizontalOffset = -0.06
+    verticalOffset = -0.08
 
     torque = model(latcontrol_inputs.lateral_acceleration, sigmoidSharpness, sigmoidTorqueGain, latAccelFactor, horizontalOffset, verticalOffset)
 
